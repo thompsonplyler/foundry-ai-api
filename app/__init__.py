1c6eea39bb57e1c71e915f1ac0398c40c784c45f
Thompson Plyler <nealok.g@gmail.com>
2024-12-31 12:38:03 -0500

    Moved routes to routes.py and updated init

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, world!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+import app.routes
